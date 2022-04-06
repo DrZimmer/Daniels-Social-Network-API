9b6e6be1176a352423d3319fb10b9aff58747514
White Tiger <dayley.zimmermann@gmail.com>
2022-04-06 13:21:33 -0400

    routes and functions defined, time to build all logic in controllers

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+
+const {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  addNewReaction,
+  removeReaction
+} = require('../../controllers/thought-controller');
+
+// /api/users
+router
+  .route('/')
+  .get(getAllThoughts);
+
+// /api/thoughts/:userId
+router
+  .route('/:userId')
+  .post(createThought);
+
+// /api/users/:id
+router
+  .route('/:id')
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router
+  .route('/:thoughtId/reactions')
+  .post(addNewReaction)
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router
+  .route('/:thoughtId/reactions/:reactionId')
+  .delete(removeReaction);
+
+module.exports = router;
